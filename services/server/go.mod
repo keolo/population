77bff6d6773a28fd53e4bcddb7ac4fa730f62b60
Keolo Keagy <keolo@Keolos-MacBook-Pro.local>
2020-03-18 16:44:47 -0700

    Organized file structure.

---
+++ services/server/go.mod
@@ -0,0 +1,8 @@
+module github.com/keolo/population/services/server
+
+go 1.13
+
+require (
+	go.etcd.io/bbolt v1.3.3
+	golang.org/x/sys v0.0.0-20200317113312-5766fd39f98d // indirect
+)
---
+++ services/server/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+func main() {
+	// Set up database connection.
+	db, err := setupDB()
+	if err != nil {
+		fmt.Errorf("error opening db", err)
+	}
+	defer db.Close()
+
+	// Start server.
+	log.Print("server started")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		showMetroStatHandler(w, r, db)
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func showMetroStatHandler(w http.ResponseWriter, r *http.Request, db *bolt.DB) {
+	log.Print("received request")
+	zip := r.URL.Path[1:]
+
+	// Retrieve population data for a given zip code.
+	val := fetch(zip, db)
+	fmt.Fprint(w, string(val))
+}
+
+// setupDB opens a database connection.
+func setupDB() (*bolt.DB, error) {
+	db, err := bolt.Open("services/server/population.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return nil, fmt.Errorf("could not open db, %v", err)
+	}
+	log.Print("db setup done")
+	return db, nil
+}
+
+// fetch returns the value of a matching key in a given bolt database.
+// * Retrieve key/zip from db
+// * Return value (json string)
+// * Return empty json object if not found
+func fetch(zip string, db *bolt.DB) []byte {
+	bucketName := []byte("MetroStat")
+	key := []byte(zip)
+	var val []byte
+
+	log.Print("looking up key:", string(key))
+
+	err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(bucketName)
+		if bucket == nil {
+			return fmt.Errorf("bucket not found: %q", bucketName)
+		}
+		val = bucket.Get(key)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(val) == 0 {
+		val = []byte("Empty!")
+	} else {
+		val = val
+	}
+
+	return val
+}
